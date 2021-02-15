762474ecee9a1ecf7649936e9909c7c7f85a40b8
unknown <maksgovruha@gmail.com>
2021-02-15 23:37:12 +0200

    Small changes

--- test-place.js
+++ test-place.js
@@ -3,9 +3,9 @@
 const gauss = require('gaussian-elimination');
 
 const arr = [
-  [1, 2, -1, 9],
-  [2, -1, 3, 13],
-  [3, 2, -5, -1]
+  [0, 2, -1, 9],
+  [2, 0, 3, 13],
+  [3, 2, 0, -1]
 ];
 
 const res = gauss(arr);
