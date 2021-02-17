e51a1d09115f25e28c8f7bbf42ae3a37d2a17281
unknown <maksgovruha@gmail.com>
2021-02-17 20:29:43 +0200

    Add matrix msx finder

--- lab1/gauss-method.js
+++ lab1/gauss-method.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const alpha = 3;
-
 const matrixA = [
   [ 3.81, 0.25, 1.28, 3.75 ],
   [ 2.25, 1.32, 7.58, 0.49 ],
@@ -16,9 +14,38 @@ const vectorB = [
   13.48,
 ];
 
+const fns = {};
+
+fns.mod = Math.abs;
+
+fns.maxItem = (matrix) => {
+  const res = { i: 0, j: 0, koff: 0 };
+
+  for (let i = 0; i < matrix.length; i++) {
+    const max = matrix[i].reduce((a, b) => 
+    fns.mod(a) > fns.mod(b) ? a : b);
+    
+    if (max > res.koff) {
+      res.koff = max;
+      res.j = i;
+    };
+  }
+
+  res.i = matrix[res.j].indexOf(res.koff);
+  
+  return res;
+};
+
 const gauss = (matrix, vector) => {
   const length = matrix.length;
   console.log(length);
-}
 
+
+  for (let i = 0; i < length; i++) {
+
+  }
+};
+
+
+console.log(fns.maxItem(matrixA));
 gauss(matrixA);
