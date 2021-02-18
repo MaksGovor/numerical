5a733539ff8fcde79aa9d24f368c284e9884053f
unknown <maksgovruha@gmail.com>
2021-02-18 15:58:09 +0200

    Add vector to alg

--- lab1/gauss-method.js
+++ lab1/gauss-method.js
@@ -14,6 +14,18 @@ const vectorB = [
   13.48,
 ];
 
+const matrix = [
+  [1, 2, -1],
+  [2, -1, 3],
+  [3, 2, -5],
+];
+
+const vector = [
+  1,
+  2,
+  3,
+]
+
 const fns = {};
 
 fns.mod = Math.abs;
@@ -25,7 +37,7 @@ fns.maxItem = (matrix) => {
     const max = matrix[i].reduce((a, b) =>
       (fns.mod(a) > fns.mod(b) ? a : b));
 
-    if (max > res.num) {
+    if (fns.mod(max) > fns.mod(res.num)) {
       res.num = max;
       res.iN = i;
     }
@@ -47,6 +59,7 @@ fns.matrixCopy = (matrix) => {
 
 fns.swapRows = (matrix, row1, row2) => {
   if (row1 === row2) return;
+  console.log(`ROWS: ${row1}<->${row2}`);
   const copyRow1 = matrix[row1];
   matrix[row1] = matrix[row2];
   matrix[row2] = copyRow1;
@@ -54,6 +67,7 @@ fns.swapRows = (matrix, row1, row2) => {
 
 fns.swapCols = (matrix, col1, col2) => {
   if (col1 === col2) return;
+  console.log(`COLS: ${col1}<->${col2}`);
   for (let i = 0; i < matrix.length; i++) {
     const copyItem = matrix[i][col1];
     matrix[i][col1] = matrix[i][col2];
@@ -63,34 +77,42 @@ fns.swapCols = (matrix, col1, col2) => {
 
 const gauss = (matrix, vector) => {
   const mat = fns.matrixCopy(matrix);
+  const vec = vector.slice();
   const length = mat.length;
   const continues = [];
 
   for (let q = 0; q < length; q++) {
     let { iN, jN, num } = fns.maxItem(mat);
+    console.log({ iN, jN, q, num });
     fns.swapRows(mat, iN, q);
-    console.log({ iN, jN, q });
+    fns.swapRows(vec, iN, q);
     fns.swapCols(mat, jN, q);
     iN = q;
     const mainLine = mat[iN];
     continues.push(iN);
 
+    vec[iN] = vec[iN] / num;
     for (let i = 0; i < length; i++) {
       mainLine[i] = mainLine[i] / num;
     }
 
-    for (let i = 0; i < length; i++) {
+    for (let i = q; i < length; i++) {
       if (continues.includes(i)) continue;
       const koff = mat[i][q];
       const copyML = mainLine.slice().map((x) => x * koff);
+      vec[i] = vec[i] - vec[iN] * koff;
       for (let j = q; j < length; j++) {
         mat[i][j] = mat[i][j] - copyML[j];
       }
     }
-  console.log(mat);
-
+  console.log('/');
+  console.log(mat[0]);
+  console.log(mat[1]);
+  console.log(mat[2]);
+  console.log('\\');
+  console.log(vec);
   }
-
 };
 
-gauss(matrixA);
+console.log(matrix);
+gauss(matrix, vector);
