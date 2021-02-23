e8e1bcfebce49a9102e994c78b0cd4ce42f4fb1d
unknown <maksgovruha@gmail.com>
2021-02-23 21:09:04 +0200

    Code style fix

--- lab1/fns.js
+++ lab1/fns.js
@@ -6,8 +6,19 @@ fns.mod = Math.abs;
 
 fns.roundPlus = (accuracy, num) => {
   if (isNaN(num) || isNaN(accuracy)) return false;
-  const m = Math.pow(10, accuracy);
-  return Math.round(num * m) / m;
+  const p = Math.pow(10, accuracy);
+  const mantisa = Math.round(num * p);
+  return mantisa / p;
+};
+
+fns.significantRound = (accuracy, num) => {
+  const strNum = num.toString().split('.')[1];
+  let acc = 0;
+  for (const s of strNum) {
+    if (s !== '0') break;
+    acc++;
+  }
+  return fns.roundPlus(accuracy + acc, num);
 };
 
 fns.partial = (fn, ...args) => (...rest) => fn(...args, ...rest);
