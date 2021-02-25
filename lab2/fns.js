fdb05f01b68d9910ac4409c094995a15ff302388
unknown <maksgovruha@gmail.com>
2021-02-25 13:40:16 +0200

    Add stub

---
+++ lab2/fns.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const fns = {};
+
+fns.mod = Math.abs;
+
+fns.roundPlus = (accuracy, num) => {
+  if (isNaN(num) || isNaN(accuracy)) return false;
+  const p = Math.pow(10, accuracy);
+  const mantisa = Math.round(num * p);
+  return mantisa / p;
+};
+
+fns.significantRound = (accuracy, num) => {
+  const strNum = num.toString().split('.')[1];
+  if (!strNum) return num;
+  let acc = 0;
+  for (const s of strNum) {
+    if (s !== '0') break;
+    acc++;
+  }
+  return fns.roundPlus(accuracy + acc, num);
+};
+
+fns.partial = (fn, ...args) => (...rest) => fn(...args, ...rest);
+
+fns.matrixCopy = (matrix) => {
+  const mat = matrix.slice();
+  for (let i = 0; i < matrix.length; i++) {
+    mat[i] = mat[i].slice();
+  }
+
+  return mat;
+};
+
+fns.swapRows = (matrix, row1, row2) => {
+  if (row1 === row2) return;
+  const copyRow1 = matrix[row1];
+  matrix[row1] = matrix[row2];
+  matrix[row2] = copyRow1;
+};
+
+fns.multipyMatrix = (mat1, mat2) => {
+  const rowsA = mat1.length, colsA = mat1[0].length,
+    rowsB = mat2.length, colsB = mat2[0].length;
+  const res = [];
+  if (colsA !== rowsB) return false;
+  for (let i = 0; i < rowsA; i++) res[i] = [];
+  for (let k = 0; k < colsB; k++) {
+    for (let i = 0; i < rowsA; i++) {
+      let t = 0;
+      for (let j = 0; j < rowsB; j++) t += mat1[i][j] * mat2[j][k];
+      res[i][k] = t;
+    }
+  }
+  return res;
+};
+
+fns.sumVector = (vec1, vec2) =>  {
+  const len1 = vec1.length, len2 = vec2.length;
+  if (len1 !== len2) return false;
+  const res = [];
+  for (let i = 0; i < len1; i++) {
+    res[i] = vec1[i] + vec2[i];
+  }
+  return res;
+};
+
+module.exports = fns;
--- lab2/jacobi-method.js
+++ lab2/jacobi-method.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const fns = require('./fns');
+const { matrixA, vectorB } = require('./task.json');
+
+const jacobi = (matrix, vector) => {
+  const mat = fns.matrixCopy(matrix);
+  const vec = vector.slice();
+
+  console.log(mat, vec);
+};
+
+jacobi(matrixA, vectorB);
