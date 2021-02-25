2c595744914cb3783cd14fb5867b78e849915f49
unknown <maksgovruha@gmail.com>
2021-02-25 14:52:53 +0200

    Add bringing for iteration methods

--- lab2/jacobi-method.js
+++ lab2/jacobi-method.js
@@ -3,6 +3,27 @@
 const fns = require('./fns');
 const { matrixA, vectorB } = require('./task.json');
 
+const bringMatrix = (matrix, vector) => {
+  const len = matrix.length;
+  const matR = []; // C - in the training manual
+  const vecR = []; // d - in the training manual
+
+  for (let i = 0; i < len; i++) {
+    const divider = matrix[i][i];
+    matR[i] = [];
+    vecR[i] = vector[i] / divider;
+    for (let j = 0; j < len; j++) {
+      if (i === j) {
+        matR[i][j] = 0;
+        continue;
+      }
+      matR[i][j] = -1 * matrix[i][j] / divider;
+    }
+  }
+
+  return { matR, vecR };
+};
+
 const jacobi = (matrix, vector) => {
   const mat = fns.matrixCopy(matrix);
   const vec = vector.slice();
@@ -10,4 +31,7 @@ const jacobi = (matrix, vector) => {
   console.log(mat, vec);
 };
 
-jacobi(matrixA, vectorB);
+const a = bringMatrix(matrixA, vectorB);
+
+console.table(a.matR);
+console.table(a.vecR);
