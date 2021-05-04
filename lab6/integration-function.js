361afa658bd7af3c420d7a49af561c2f53c35f6a
MaksGovor <maksgovruha@gmail.com>
2021-05-04 12:32:51 +0300

    Fix bug

--- lab6/integration-function.js
+++ lab6/integration-function.js
@@ -19,7 +19,7 @@ const simpsonMethod = (func, count, interval) => {
   
   let sum = 0;
   for (let i = 0; i < yValues.length / 2; i++) {
-    sum += 2 * yValues[2 * i] + yValues[2 * i + 1];
+    sum += 2 * yValues[2 * i] + (yValues[2 * i + 1] || 0);
   }
 
   sum = sum * 2 + func(leftP) + func(rightP);
@@ -28,7 +28,7 @@ const simpsonMethod = (func, count, interval) => {
 }
 
 
-const res1 = trapeziumMethod(func, 100, [-0.7, 1.2]);
-const res2 = simpsonMethod(func, 100, [-0.7, 1.2]);
-console.log(res1);
+// const res1 = trapeziumMethod(func, 100, [-0.7, 1.2]);
+const res2 = simpsonMethod(func, 50, [-0.7, 1.2]);
+// console.log(res1);
 console.log(res2);
