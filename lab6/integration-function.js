1db5630069f1387e232021d134f332a16de5fa9f
MaksGovor <maksgovruha@gmail.com>
2021-05-05 14:00:11 +0300

    add gauss method

--- lab6/integration-function.js
+++ lab6/integration-function.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const func = require('./function');
+const { quadraticCoeffs } = require('./task.json');
 
 const trapeziumMethod = (func, count, interval) => {
   const [ leftP, rightP ] = interval;
@@ -27,8 +28,26 @@ const simpsonMethod = (func, count, interval) => {
   return sum * step / 3;
 }
 
+const gaussMethod = (func, count, interval) => {
+  const [ leftP, rightP ] = interval;
+  const funcM = !(leftP === -1 && rightP === 1) ?
+    (t) => (rightP - leftP) * func(((rightP + leftP) + (rightP - leftP) * t) / 2) / 2 : 
+    func;
+  
+  if (!quadraticCoeffs[count]) return 'No coffs';
+  const { x, A } = quadraticCoeffs[count];
+
+  let sum = 0;
+  for (let i = 0; i < x.length; i++) {
+    sum += funcM (x[i]) * A[i];
+  }
+
+  return sum;
+}
 
 // const res1 = trapeziumMethod(func, 100, [-0.7, 1.2]);
-const res2 = simpsonMethod(func, 50, [-0.7, 1.2]);
+// const res2 = simpsonMethod(func, 50, [-0.7, 1.2]);
 // console.log(res1);
-console.log(res2);
+
+const res3 = gaussMethod(func, 6, [-0.7, 1.2]);
+console.log(res3);
