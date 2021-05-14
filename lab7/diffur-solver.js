54094279d693996e1ce3344c2712320551907f9d
MaksGovor <maksgovruha@gmail.com>
2021-05-14 23:54:42 +0300

    add func

--- lab7/diffur-solver.js
+++ lab7/diffur-solver.js
@@ -1,3 +1,12 @@
 'use strict';
 
 const yD = require('./function');
+
+const rungeKuttmethod = (yD, interval, step, y0) => {
+  const [ x0, right ] = interval;
+  const result = [ [x0, y0] ];
+
+  for (let xi = x0; xi <= right; x += step) {
+    
+  }
+}
