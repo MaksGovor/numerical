ed44b2b575e5f1704b26df4003ceaa0278142d92
MaksGovor <maksgovruha@gmail.com>
2021-05-15 13:34:33 +0300

    Correct mustakes

--- lab7/diffur-solver.js
+++ lab7/diffur-solver.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const yD = require('./function');
+const eps = 10 ** (-1);
 
 const roundPlus = (accuracy, num) => {
   if (isNaN(num) || isNaN(accuracy)) return false;
@@ -9,21 +10,28 @@ const roundPlus = (accuracy, num) => {
   return mantisa / p;
 };
 
-const rungeKuttmethod = (yD, interval, step, y0) => {
+const rungeKuttmethod = (yD, interval, step, y0, points) => {
   const [ x0, right ] = interval;
   const result = [ [x0, y0] ];
   let stepH = step;
 
-  let yi = y0;
-  for (let xi = x0; xi <= right - stepH; xi += stepH) {
+  let yi = y0, count = 1;
+  for (let xi = x0; xi <= right - stepH;) {
     const k1 = stepH * yD(xi, yi);
     const k2 = stepH * yD(xi + stepH / 2, yi + k1 / 2);
     const k3 = stepH * yD(xi + stepH / 2, yi + k2 / 2);
     const k4 = stepH * yD(xi + stepH, yi + k3);
 
     const deltaY = (k1 + 2 * (k2 + k3) + k4) / 6;
+    // console.table({ k1, k2, k3, k4, deltaY });
     yi += deltaY;
     result.push([ roundPlus(6, xi + stepH), yi ]);
+
+    xi += stepH;
+    if (Math.abs((k2 - k3) / (k1 - k2)) > eps) stepH /= 2;
+    
+    count++;
+    if (points && count === points) break;
   }
 
   return result;
